be3d596df3ad80148524212456fad063a18f72a6
Basia <bzogala@poczta.onet.pl>
2014-03-11 14:49:02 +0100

    create devel and add test for removing turtle

---
+++ devel/turtle_undo_tests.R
@@ -0,0 +1,42 @@
+library(grid)
+n <- 1000
+ruchy <- data.frame(x = runif(n), y = runif(n))
+
+cat("Adding and removing turtle at each step - grid package:\n")
+system.time(expr={
+  grid.newpage()
+  grid.circle(ruchy$x[1], ruchy$y[1], r = .05, name = "ciri")
+  for(i in 2:(n-1)){
+    grid.remove("ciri")
+    grid.polygon(ruchy$x[i:(i+1)], ruchy$y[i:(i+1)], name = "linie")
+    grid.circle(ruchy$x[i], ruchy$y[i], r = .05, name = "ciri")
+    cat(sprintf("\r%f", i/n))
+  }
+  cat("\r")
+})
+
+cat("Creating standard plot at each step:\n")
+system.time(expr={
+  for(i in 1:n){
+    plot(ruchy$x[1:i], ruchy$y[1:i], type = "l", axes = F, xlab = "", ylab = "")
+    points(ruchy$x[i], ruchy$y[i], cex = 3)
+    cat(sprintf("\r%f", i/n))
+  }  
+  cat("\r")
+})
+
+library(iplots)
+
+cat("Adding and removing turtle at each step - iplots package:\n")
+system.time(expr={
+  iplot(c(0,ruchy$x[1]), c(0,ruchy$y[1]),xlim = c(0,1), ylim = c(0,1))
+  itext(ruchy$x[1], ruchy$y[1], "zolw")
+  for(i in 2:(n-1)){
+    iobj.rm()
+    ilines(ruchy$x[i:(i+1)], ruchy$y[i:(i+1)])
+    itext(ruchy$x[i], ruchy$y[i], "zolw")
+    cat(sprintf("\r%f", i/n))
+  }
+  cat("\r")
+})
+
