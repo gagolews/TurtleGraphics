f9739f673a91d90e3df28aafa904ef27548dc261
Basia <bzogala@poczta.onet.pl>
2014-03-12 23:15:20 +0100

    added Marcins latticeExtra to my tests

--- devel/turtle_undo_tests.R
+++ devel/turtle_undo_tests.R
@@ -1,9 +1,9 @@
 library(grid)
-n <- 1000
+n <- 200
 ruchy <- data.frame(x = runif(n), y = runif(n))
 
 cat("Adding and removing turtle at each step - grid package:\n")
-system.time(expr={
+x <- system.time(expr={
   grid.newpage()
   grid.circle(ruchy$x[1], ruchy$y[1], r = .05, name = "ciri")
   for(i in 2:(n-1)){
@@ -14,9 +14,10 @@ system.time(expr={
   }
   cat("\r")   ### can any1 tell me why this line is necessary? Marcin
 })
+print(x)
 
 cat("Creating standard plot at each step:\n")
-system.time(expr={
+x <- system.time(expr={
   for(i in 1:n){
     plot(ruchy$x[1:i], ruchy$y[1:i], type = "l", axes = F, xlab = "", ylab = "")
     points(ruchy$x[i], ruchy$y[i], cex = 3)
@@ -24,11 +25,11 @@ system.time(expr={
   }  
   cat("\r")
 })
+print(x)
 
 library(iplots)
-
 cat("Adding and removing turtle at each step - iplots package:\n")
-system.time(expr={
+x <- system.time(expr={
   iplot(c(0,ruchy$x[1]), c(0,ruchy$y[1]),xlim = c(0,1), ylim = c(0,1))
   itext(ruchy$x[1], ruchy$y[1], "zolw")
   for(i in 2:(n-1)){
@@ -39,4 +40,26 @@ system.time(expr={
   }
   cat("\r")
 })
+print(x)
+
+library(latticeExtra)
+cat("Adding and removing turtle at each step - latticeExtra package:\n")
+x <- system.time(expr = {
+  coor <- c(0,0)
+  trellis.par.set("axis.line",list(col=NA,lty=1,lwd=1)) 
+  Graph <- xyplot(0~0, xlab="", ylab="", type="l", 
+                  scales=list(draw=FALSE),subscripts = TRUE) 
+  Graph + layer({panel.text(coor[1], coor[2], "-O-") })
+  n_coor <- as.data.frame(matrix(round(runif(2*n) / 5, digits=3), nrow = n) )
+
+  for (i in 2:n){   
+    Graph <- Graph + layer(panel.lines(x=c(n_coor[1:i,1]),
+                                       y=c(n_coor[1:i,2])))
+    print(Graph)
+    print(Graph + layer(panel.text(x=n_coor[i,1], y=n_coor[i,2], "-O-"))  )
+    cat(sprintf("\r%f", i/n))
+  }  
+  cat("\r")
+})
+print(x)
 
