c5d4e0c7bfa6fa89b1c5e0782a496e2ed6723233
Anna Cena <a.cena@phd.ipipan.waw.pl>
2014-04-01 21:03:31 +0200

    turtle_init, move_forward v03

--- devel/Ania/turtle_init.R
+++ devel/Ania/turtle_init.R
@@ -1,8 +1,11 @@
 require("grid")
 library("png")
 
-# I tink that we should create the file img and put turtle2.img inside - thats how it was done 
-# in png package
+# I think that we should create the directory img and put turtle2.img inside - 
+# thats how it was done in png package
+
+# w pliku utworzona zmienna tym co funkcja bedzie dostepna w calym namespace pakietu i 
+# mozna ja zmieniac <<-
 
 show_turtle<-function(x, y){
   image <- readPNG("devel/Ania/turtle2.png")
@@ -19,12 +22,13 @@ turtle_init<- function(){
   show_turtle(ruchy$x, ruchy$y)
 }
 
-move_forward<- function(x){
+move_forward<- function(x, col="black", lwd=1, lty=1){
   n <- nrow(ruchy)
   y <- ruchy$y[n]+x
-  hide_turtle()
   ruchy <<- rbind(ruchy, c(ruchy[n, 1], y))
-  grid.polygon(ruchy$x[n:(n+1)], ruchy$y[n:(n+1)], name = "lines")
+  hide_turtle()
+  grid.polygon(ruchy$x[n:(n+1)], ruchy$y[n:(n+1)], name = "lines", 
+               gp=gpar(col=col,lwd=lwd, lty=lty))
   show_turtle(ruchy$x[n+1], ruchy$y[n+1])
 }
 
@@ -32,8 +36,6 @@ graphics.off()
 
 turtle_init()
 move_forward(0.2)
-move_forward(0.1)
-move_forward(0.1)
-
-
+move_forward(0.1, col="red", lty=2)
+move_forward(0.1, col="blue", lty=3)
 
