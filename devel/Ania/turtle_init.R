bccbbe0ef65d7cfac912759c630b78ffb37211fe
Anna Cena <a.cena@phd.ipipan.waw.pl>
2014-04-01 20:31:01 +0200

    turtle_init - clean up

--- devel/Ania/turtle_init.R
+++ devel/Ania/turtle_init.R
@@ -1,25 +1,31 @@
 require("grid")
 library("png")
-# help(package="grid")
-
-turtle_init<- function(){
-  image <- readPNG("devel/Ania/turtle2.png")
-  ruchy <<- data.frame(x=0.5, y=0.5)
-  grid.newpage()
-  grid.raster(as.raster(image), x=0.5, y=0.5, height=unit(0.09,"npc"), width=unit(0.09,"npc"), name="turtle")
-}
 
 # I tink that we should create the file img and put turtle2.img inside - thats how it was done 
 # in png package
 
-move_forward<- function(x){
-  y <- x+0.5
-  n <- nrow(ruchy)
+show_turtle<-function(x, y){
   image <- readPNG("devel/Ania/turtle2.png")
-  ruchy <<- rbind(ruchy, c(ruchy[n, 1], y))
+  grid.raster(as.raster(image), x, y, height=unit(0.09,"npc"), width=unit(0.09,"npc"), name="turtle")
+}
+
+hide_turtle<-function(){
   grid.remove("turtle")
+}
+
+turtle_init<- function(){
+  ruchy <<- data.frame(x=0.5, y=0.5)
+  grid.newpage()
+  show_turtle(ruchy$x, ruchy$y)
+}
+
+move_forward<- function(x){
+  n <- nrow(ruchy)
+  y <- ruchy$y[n]+x
+  hide_turtle()
+  ruchy <<- rbind(ruchy, c(ruchy[n, 1], y))
   grid.polygon(ruchy$x[n:(n+1)], ruchy$y[n:(n+1)], name = "lines")
-  grid.raster(as.raster(image), x=ruchy$x[n+1], y=ruchy$y[n+1], height=unit(0.09,"npc"), width=unit(0.09,"npc"), name="turtle")
+  show_turtle(ruchy$x[n+1], ruchy$y[n+1])
 }
 
 graphics.off()
