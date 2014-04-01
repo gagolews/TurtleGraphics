950a2a7bdd29f1b59a212fd44cc9d176c086d519
Anna Cena <a.cena@phd.ipipan.waw.pl>
2014-04-01 20:16:18 +0200

    turtle_init v02

--- devel/Ania/turtle_init.R
+++ devel/Ania/turtle_init.R
@@ -3,12 +3,14 @@ library("png")
 # help(package="grid")
 
 turtle_init<- function(){
+  image <- readPNG("devel/Ania/turtle2.png")
   ruchy <<- data.frame(x=0.5, y=0.5)
   grid.newpage()
-#   plot.window(c(-1,1), c(-1,1))
-#   grid.circle(ruchy$x[1], ruchy$y[1], r = .01, name = "ciri")
   grid.raster(as.raster(image), x=0.5, y=0.5, height=unit(0.09,"npc"), width=unit(0.09,"npc"), name="turtle")
 }
+
+# I tink that we should create the file img and put turtle2.img inside - thats how it was done 
+# in png package
 
 move_forward<- function(x){
   y <- x+0.5
@@ -16,12 +18,15 @@ move_forward<- function(x){
   ruchy <<- rbind(ruchy, c(ruchy[n, 1], y))
   grid.remove("turtle")
   grid.polygon(ruchy$x[n:(n+1)], ruchy$y[n:(n+1)], name = "lines")
-#   grid.circle(ruchy$x[n+1], ruchy$y[n+1], r = .01, name = "ciri")
   grid.raster(as.raster(image), x=ruchy$x[n+1], y=ruchy$y[n+1], height=unit(0.09,"npc"), width=unit(0.09,"npc"), name="turtle")
 }
 
-graphics.off()
-image <- readPNG("devel/Ania/turtle2.png")
-
 turtle_init()
-move_forward(0.1)
+move_forward(0.7)
+
+help(package="png")
+
+graphics.off()
+
+grid.polygon(x=unit(0, "npc"), y=unit(0, "npc"))
+grid.polygon(x=unit(c(0,0.5), "npc"), y=unit(c(0,0.5), "npc"))
