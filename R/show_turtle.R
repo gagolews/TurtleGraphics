8c22aee7e43311d98df287e39e1427f857bcc785
Anna Cena <a.cena@phd.ipipan.waw.pl>
2014-04-04 12:15:06 +0200

    Corrected init, show and hide

--- R/show_turtle.R
+++ R/show_turtle.R
@@ -1,5 +1,7 @@
 #' @export
 show_turtle<-function(x, y){
-  image <- readPNG("img/turtle2.png")
-  grid.raster(as.raster(image), x, y, height=unit(0.09,"npc"), width=unit(0.09,"npc"), name="turtle")
+  grid.raster(.image, x, y, 
+              height=unit(0.09,"npc"), width=unit(0.09,"npc"), 
+              name="turtle")
 }
+
