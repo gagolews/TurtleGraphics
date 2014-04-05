3125ba390b088260b0cc30c84fa6956d4681af21
Basia <b.zogala@phd.ipipan.waw.pl>
2014-04-06 00:47:56 +0200

    add forward_tmp for testing turn and set_param

---
+++ R/forward_tmp.R
@@ -0,0 +1,37 @@
+#' @rdname forward_tmp
+#' @export
+forward_tmp <- function(moves = 1){
+  dist <- dist / 20
+  
+  # current values for .turtle_history
+  curN <- .turtle_history$N
+  curX <- .turtle_history$moves$x[curN]
+  curY <- .turtle_history$moves$y[curN]  
+  curAng <- .turtle_history$moves$angle[curN]  
+  curCol <- .turtle_history$col
+  curLwd <- .turtle_history$moves$lwd[curN]
+  curLty <- .turtle_history$moves$lty[curN]
+  
+  # new values for turtle history
+  newN <- curN %% nrow(.turtle_history$moves) + 1
+  newX <- curX + moves * sin(curAng * pi / 180)
+  newY <- curY + moves * cos(curAng * pi / 180)
+  
+  # TODO: check if turtle is in the frame otherwise warning 
+  # TODO: does not work when visible = F
+  
+  hide_turtle()
+  if(.turtle_history$moves$draw[curN])
+    grid.polygon(c(curX, newX), c(curY, newY),
+                 name = "lines", 
+                 gp = gpar(col = curCol,
+                           lwd = curLwd, 
+                           lty = curLty))
+  show_turtle(newX, newY, curAng)
+  
+  # changing .turtle_history
+  .turtle_history$N <<- newN
+  .turtle_history$moves[newN, ] <<- .turtle_history$moves[curN, ]
+  .turtle_history$moves$x[newN] <<- newX
+  .turtle_history$moves$y[newN] <<- newY
+}
