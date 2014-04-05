a08a5968f3a294ec3f158ab945aa75ddce7d0c5c
Basia <b.zogala@phd.ipipan.waw.pl>
2014-04-05 23:09:11 +0200

    add draw parameter to turtle_init

--- R/turtle_init.R
+++ R/turtle_init.R
@@ -15,7 +15,8 @@ turtle_init<- function(size=1000){
                            moves = data.frame(x = c(0.5, rep(NA, size- 1)), 
                                               y = c(0.5, rep(NA, size - 1)), 
                                               angle = c(0, rep(NA, size - 1)), 
-                                              visible = c(TRUE, rep(NA, size - 1)), 
+                                              visible = c(TRUE, rep(NA, size - 1)),
+                                              draw = c(TRUE, rep(NA, size - 1)),
                                               col = c("black", rep(NA, size - 1)), 
                                               lwd = c(1, rep(NA, size - 1)), 
                                               lty = c(1, rep(NA, size - 1))))
