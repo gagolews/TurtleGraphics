d75a183111e2f8ab2008467c6c6bcbd9fe792346
Basia <b.zogala@phd.ipipan.waw.pl>
2014-04-05 13:46:48 +0200

    set_param with up and down

--- R/set_param.R
+++ R/set_param.R
@@ -1,26 +1,43 @@
 # sets parameters of a turtle: col, lty, lwd 
 # TODO: chceck correctness of parameters
 
-set_param <- function(col = NA, lwd = NA, lty = NA) {
-  if(!exists("turtle_history")) 
+#' @export
+set_param <- function(col = NA, lwd = NA, lty = NA, visible = NA) {
+  if(!exists(".turtle_history")) 
     stop("Turtle has not been initiated, please type turtle_init() first")
-  if(all(is.na(col), is.na(lwd), is.na(lty))) 
-    stop("You need to give at least one parameter to set: 'col', 'lwd', 'lty'")
-  
-  if(!is.na(col)) turtle_history$moves$col[turtle_history$N] <<- col
-  if(!is.na(lwd)) turtle_history$moves$lwd[turtle_history$N] <<- lwd
-  if(!is.na(lty)) turtle_history$moves$lty[turtle_history$N] <<- lty
+  if(all(is.na(col), is.na(lwd), is.na(lty), is.na(visible))) 
+    stop("You need to give at least one parameter to set: 'col', 'lwd', 'lty', 'visible")
+
+  curN <- .turtle_history$N
+  if(!is.na(col)) .turtle_history$moves$col[curN] <<- col
+  if(!is.na(lwd)) .turtle_history$moves$lwd[curN] <<- lwd
+  if(!is.na(lty)) .turtle_history$moves$lty[curN] <<- lty
+  if(!is.na(visible)) .turtle_history$moves$visible[curN] <<- visible  
   
 }
 
+#' @export
 set_col <- function(col) {
   set_param(col = col)
 }
 
+#' @export
 set_lwd <- function(lwd) {
   set_param(lwd = lwd)
 }
 
+#' @export
 set_lty <- function(lty) {
   set_param(lty = lty)
 }
+
+#' @export
+up <- function() {
+  set_param(visible = FALSE)
+}
+
+#' @export
+down <- function() {
+  set_param(visible = TRUE)
+}
+
